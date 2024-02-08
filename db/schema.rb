b93ad05fed2f4c7af44156c635b39d661d2df553
Alexandra Cambiaso <acambiaso93@gmail.com>
2024-02-08 18:55:38 +0100

    added migration Address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_181259) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_173652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_06_181259) do
     t.boolean "available_for_rent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
