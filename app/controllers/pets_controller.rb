6ae6aa48721851e57a3dc01b812c683801562311
Alexandra Cambiaso <acambiaso93@gmail.com>
2024-02-08 20:18:56 +0100

    working on creating a booking

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,10 +1,10 @@
 class PetsController < ApplicationController
-<<<<<<< HEAD
   def show
     @pet = Pet.find(params[:id])
-=======
+    @booking = Booking.new
+  end
+
   def index
     @pets = Pet.all
->>>>>>> ba6fdc51df28dab868eb91845d14b85bbed13966
   end
 end
