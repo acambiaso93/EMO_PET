db5d240131a9e9a0361074b9fe25912ead7ff082
Jakub Danilewicz <jakubdanilewicz@gmail.com>
2024-02-08 20:18:59 +0100

    Form added

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,8 @@
 class Pet < ApplicationRecord
   has_many :booking
+
+  validates :name, presence: true
+  validates :breed, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
 end
