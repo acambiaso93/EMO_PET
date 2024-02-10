9a1b368e109afa26b729e9ecc920d18fc2df29fd
MAximilianRaine <raine.maximilian@gmail.com>
2024-02-10 16:43:44 +0100

    delete a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_booking, only: [:destroy]
 
   def show
     @bookings = Booking.find(params[:id])
@@ -8,5 +9,16 @@ class BookingsController < ApplicationController
 
   def index
     @my_bookings = current_user.bookings
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_url, notice: 'Booking was successfully destroyed.'
+  end
+
+  private
+
+  def set_booking
+    @booking = current_user.bookings.find(params[:id])
   end
 end
