4e36b377df30b03be831d8a96b72449c943657b0
Alexandra Cambiaso <acambiaso93@gmail.com>
2024-02-17 14:33:52 +0100

    dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,8 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
+
   def show
     @my_pets = current_user.pets
     @my_bookings = current_user.bookings
